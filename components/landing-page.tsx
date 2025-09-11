1b8dac1c521145f244d077055e2d5fa4859b490e
WojtekZaniewski <wojtekzaniewski@icloud.com>
2025-09-11 22:51:35 +0200

    Update landing-page.tsx

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import React, { useState } from "react"
 
 export default function LandingPage() {
   const [clickedNav, setClickedNav] = useState<string | null>(null)
